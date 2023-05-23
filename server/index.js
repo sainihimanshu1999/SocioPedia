0e72f5aeac815242ff60cf581c242a17c3ef4b3c
sainihimanshu1999 <sainihimanshu.1999@gmail.com>
2023-05-23 18:51:42 +0530

    backend complete

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from 'path'
 import { fileURLToPath } from "url"
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
 import {register} from './controllers/auth.js'
+import {createPost} from './controllers/posts.js'
+import { verifyToken } from "./middleware/auth.js"
 
 //Configurations
 
@@ -44,11 +47,13 @@ const upload = multer({storage})
 //Routes with files
 
 app.post('/auth/register', upload.single('picture'), register)
+app.post('/posts', verifyToken, upload.single('picture'), createPost)
 
 //Seperate Routes
 
 app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
+app.use('/posts', postRoutes)
 
 
 //Mongoose Setup
